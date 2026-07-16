c34211d68c0b7f35bb6f63e6f2c66da8daabb51c
Ronald Amancio <ronsynt@gmail.com>
2026-07-17 05:23:47 +0800

    feature: Added IVRFlow Controller, Blob Storage, MenuRenderer

---
+++ KolHaNitzachon.PhoneSystem/KolHaNitzachon.PhoneSystem.API/Controllers/IVRFlowController.cs
@@ -0,0 +1,27 @@
+﻿using KolHaNitzachon.PhoneSystem.Application.Interfaces.IVR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KolHaNitzachon.PhoneSystem.API.Controllers
+{
+    [ApiController]
+    [Route("api/ivr")]
+    public class IVRFlowController : ControllerBase
+    {
+        private readonly IMenuRenderer _menuRenderer;
+
+        public IVRFlowController(IMenuRenderer menuRenderer)
+        {
+            _menuRenderer = menuRenderer;
+        }
+
+        [HttpPost("handle-call")]
+        public IActionResult HandleCall(
+            [FromForm] string? Digits)
+        {
+            var response = _menuRenderer.RenderMainMenu(Digits);
+
+            return Content(response.ToString(),"text/xml");
+        }
+    }
+}
